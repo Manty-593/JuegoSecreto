0efeaecc2ed35d190d6d9cf599e5244271f639f6
Manty-593 <andygt10@outlook.es>
2025-07-28 19:48:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ let numeroSecreto = 0;
 let intentos = 0;
 let listaNumerosSorteados = [];
 let numeroMaximo = 10;
-let partidasMaximas = 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
